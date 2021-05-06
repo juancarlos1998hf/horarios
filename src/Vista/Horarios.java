7a42b9a50e66d5dec90b741c683c2b51bc290e6a
Servidor <Servidor@Servidor-PC>
2021-05-06 13:55:06 -0500

    Probando.

---
+++ src/Vista/Horarios.java
@@ -0,0 +1,13 @@
+
+package Vista;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+
+public class Horarios extends JFrame {
+    
+    
+}
